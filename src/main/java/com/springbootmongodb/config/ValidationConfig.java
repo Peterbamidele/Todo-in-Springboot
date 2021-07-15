c269aec4c924be050d9e6353e028e972dcfb85b6
Peterbamidele <peterbamidele@gmail.com>
2021-07-15 05:20:00 +0100

    custtom exception add

---
+++ src/main/java/com/springbootmongodb/config/ValidationConfig.java
@@ -0,0 +1,20 @@
+package com.springbootmongodb.config;
+
+import org.apache.el.util.Validation;
+import org.springframework.boot.autoconfigure.ldap.embedded.EmbeddedLdapProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class ValidationConfig {
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener(){
+                return new ValidatingMongoEventListener(validator());
+    }
+    @Bean
+    public LocalValidatorFactoryBean validator(){
+        return new LocalValidatorFactoryBean();
+    }
+}
--- src/main/java/com/springbootmongodb/model/TodoDTO.java
+++ src/main/java/com/springbootmongodb/model/TodoDTO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Data
@@ -11,9 +12,15 @@ import java.util.Date;
 public class TodoDTO {
     @Id
     private String id;
+    @NotNull(message = "todo cannot be null")
     private String todo;
+
+    @NotNull(message = "description cannot be null")
     private String description;
+
+    @NotNull(message = "completed cannot be null ")
     private Boolean completed;
+
     private Date createdAt;
     private Date updated;
 }
